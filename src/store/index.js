22184257676569c008cb77665cdd43ae4e8ce726
Swapnil Bhattacharya <swapnilb050203@gmail.com>
2025-01-25 01:03:34 +0530

    define initial state, empty default values for our model

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import { proxy } from "valtio";
 // ?DEFINE INITIAL STATE - EMPTY DEFAULT VALUES
 const state = proxy({
     intro: true,
-    color: "#EFBD48",
+    color: "#FBFCFA",
     isLogoTexture: true,
     isFullTexture: false,
     logoDecal: "./threejs.png",
